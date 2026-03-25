2df512608458c76519be6da1b74c0e8657baa9cb
harshil-dave-mnscorp <harshil.dave@mnscorp.net>
2026-03-25 17:39:11 +0530

    Waiting for Asset workflow changes

--- src/test/java/runners/TestRunner.java
+++ src/test/java/runners/TestRunner.java
@@ -6,13 +6,15 @@ import io.cucumber.testng.CucumberOptions;
 @CucumberOptions(
         features = "src/test/resources/features/WaitingForAsset-WA.feature",
         glue = { "stepDefinitions", "hooks", "support" },
-        tags = "@consignment_step_approvals_WA",
+        tags = "@consignment_pdx_login_WA or @consignment_pdx_import_excel_WA or @consignment_step_login_WA" +
+                "@consignment_step_waitingForAsset_WA or @consignment_pdx_upload_image_WA or @consignment_step_approvals_WA",
 
         plugin = {
                 "pretty",
-                "com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:",  // ✅ Extent
                 "json:target/cucumber-reports/cucumber.json",  // ✅ for Masterthought
+                "html:target/cucumber-reports.html",
         },
         monochrome = true
 )
+
 public class TestRunner extends AbstractTestNGCucumberTests { }
