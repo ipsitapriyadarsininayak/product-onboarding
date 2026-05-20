7867a8ddb939d2473e03b650eec810c666874eea
harshil-dave-mnscorp <harshil.dave@mnscorp.net>
2026-05-20 17:48:30 +0530

    updates in waiting for asset

--- src/main/java/pages/ReworkSize.java
+++ src/main/java/pages/ReworkSize.java
@@ -702,7 +702,6 @@ public class ReworkSize extends BasePage {
         {
             System.out.println("✅ All Buyer Approval products processed. Proceeding to Asset Approval...");
         }
-
         {
 //            System.out.println("⚠️ Some Buyer Approval products were skipped or failed. Proceeding to Asset Approval...");
         }
